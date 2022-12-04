4919a1a81d6b987358eebd4c5c6b400b9b7a244e
Dominik Toth <dominik8655@gmail.com>
2022-12-04 20:42:40 +0100

    feat: add basic template files

--- README.md
+++
@@ -1,5 +0,0 @@
-# My Advent of Code Solutions
-
-Change branches to see the solutions to each day
-
-### Enjoy!
---
+++ day.ts
@@ -0,0 +1,5 @@
+const input: string = await Deno.readTextFile("input.test");
+
+console.log(input);
+
+export {};
