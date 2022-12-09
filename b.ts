b79d6b8c3b54b042b3a099a207aa425b99daa49a
Dominik Toth <dominik8655@gmail.com>
2022-12-09 18:22:03 +0100

    feat: Add solution for day #2: a, b

--- b.ts
+++ b.ts
@@ -1,3 +1,41 @@
-const input = await Deno.readTextFile("input.test");
+const input = await Deno.readTextFile("./input.prod");
 
-console.log(input);
+const lines = input.split("\n");
+lines.pop();
+const parsed = lines.map((line) => line.split(" ") as [string, string]);
+
+let score = 0;
+const translate = {
+  X: 0,
+  Y: 3,
+  Z: 6,
+};
+const translate2 = {
+  A: "C",
+  B: "A",
+  C: "B",
+};
+const translate3 = Object.fromEntries(
+  Object.entries(translate2).map(([k, v]) => [v, k])
+);
+const translate4 = {
+  A: 1,
+  B: 2,
+  C: 3,
+};
+
+parsed.forEach((item) => {
+  const [them, outcome] = item;
+
+  let me;
+  if (outcome === "Y") me = them;
+  if (outcome === "X") me = translate2[them as keyof typeof translate2];
+  if (outcome === "Z") me = translate3[them as keyof typeof translate3];
+
+  let s = translate[outcome as keyof typeof translate];
+  s += translate4[me as keyof typeof translate4];
+
+  score += s;
+});
+
+console.log(score);
--- mod.ts
+++ mod.ts
@@ -1,3 +1,37 @@
-const input = await Deno.readTextFile("input.test");
+const input = await Deno.readTextFile("./input.prod");
 
-console.log(input);
+const lines = input.split("\n");
+lines.pop();
+const parsed = lines.map((line) => line.split(" ") as [string, string]);
+
+let score = 0;
+const translate = {
+  X: "A",
+  Y: "B",
+  Z: "C",
+};
+const translate2 = {
+  A: 1,
+  B: 2,
+  C: 3,
+};
+
+parsed.forEach((item) => {
+  let [them, me] = item;
+
+  me = translate[me as keyof typeof translate];
+  let s = translate2[me as keyof typeof translate2];
+
+  if (me === them) s += 3;
+
+  if (
+    (me === "A" && them === "C") ||
+    (me === "B" && them === "A") ||
+    (me === "C" && them === "B")
+  )
+    s += 6;
+
+  score += s;
+});
+
+console.log(score);
