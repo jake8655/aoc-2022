f75d578dafd3e05a0bb4d89d0d3152442d5f18da
Dominik Toth <dominik8655@gmail.com>
2022-12-14 19:48:42 +0100

    feat: Add solution for day #4: a, b

--- b.ts
+++ b.ts
@@ -1,3 +1,16 @@
-const input = await Deno.readTextFile("input.test");
+const input = await Deno.readTextFile("./input.prod");
 
-console.log(input);
+const pairs = input.split("\n").map((line) => line.split(","));
+pairs.pop();
+
+let overlap = 0;
+
+for (const pair of pairs) {
+  const rangeA = pair[0].split("-").map((n) => +n);
+  const rangeB = pair[1].split("-").map((n) => +n);
+
+  const hasOverlap = rangeA[0] <= rangeB[1] && rangeB[0] <= rangeA[1];
+  if (hasOverlap) overlap++;
+}
+
+console.log(overlap);
--- mod.ts
+++ mod.ts
@@ -1,3 +1,16 @@
-const input = await Deno.readTextFile("input.test");
+const input = await Deno.readTextFile("input.prod");
 
-console.log(input);
+const pairs = input.split("\n").map((line) => line.split(","));
+pairs.pop();
+
+let overlap = 0;
+
+for (const pair of pairs) {
+  const [la, ra] = pair[0].split("-").map((n) => +n);
+  const [lb, rb] = pair[1].split("-").map((n) => +n);
+
+  if (la >= lb && ra <= rb) overlap++;
+  else if (la <= lb && ra >= rb) overlap++;
+}
+
+console.log(overlap);
